f2b5b831b5ef271b84c878e5319442e9d1a81100
yagcioe <yagcioe@gmail.com>
2020-06-13 00:44:08 +0200

    TODOS hinzugefügt

--- Tests/LinkedListTest.cpp
+++ Tests/LinkedListTest.cpp
@@ -7,57 +7,53 @@
 #include "Test.h"
 
 
-
-
-
-
-void testAdd(void* arg) {
+void testAdd(void *arg) {
     auto l = new GenericList<int>();
-    int* i,*j,*k,*el,*m;
+    int *i, *j, *k, *el, *m;
     int *n = new int;
-    Test::eqadr((void *) 0,(void *) l->getSize());
-    i =new int(12);
+    Test::eqadr((void *) 0, (void *) l->getSize());
+    i = new int(12);
     l->add(i);
     //l:[12]
-    Test::eqadr((void *) 12,(void *) *l->get(0));
-    Test::eqadr((void *) 1,(void *) l->getSize());
-    j =new int(13);
+    Test::eqadr((void *) 12, (void *) *l->get(0));
+    Test::eqadr((void *) 1, (void *) l->getSize());
+    j = new int(13);
     l->add(j);
     //l:[12,13]
-    Test::eqadr((void *) 12,(void *) *l->get(0));
-    Test::eqadr((void *) 13,(void *) *l->get(1));
-    Test::eqadr((void *) 2,(void *) l->getSize());
-    k =new int(14);
+    Test::eqadr((void *) 12, (void *) *l->get(0));
+    Test::eqadr((void *) 13, (void *) *l->get(1));
+    Test::eqadr((void *) 2, (void *) l->getSize());
+    k = new int(14);
     l->addFirst(k);
     //l:[14,12,13]
-    Test::eqadr((void *) 14,(void *) *l->get(0));
-    Test::eqadr((void *) 12,(void *) *l->get(1));
-    Test::eqadr((void *) 3,(void *) l->getSize());
+    Test::eqadr((void *) 14, (void *) *l->get(0));
+    Test::eqadr((void *) 12, (void *) *l->get(1));
+    Test::eqadr((void *) 3, (void *) l->getSize());
 
-    el =new int(42);
+    el = new int(42);
     //l:[14,12,13]
-    l->addAt(1,el);
+    l->addAt(1, el);
     //l:[14,42,12,13]
-    Test::eqadr((void *) 14,(void *) *l->get(0));
-    Test::eqadr((void *) 42,(void *) *l->get(1));
-    Test::eqadr((void *) 4,(void *) l->getSize());
+    Test::eqadr((void *) 14, (void *) *l->get(0));
+    Test::eqadr((void *) 42, (void *) *l->get(1));
+    Test::eqadr((void *) 4, (void *) l->getSize());
 
-    m= new int(1337);
+    m = new int(1337);
     //l:[14,42,12,13]
-    l->addAt(l->getSize(),m);
+    l->addAt(l->getSize(), m);
     //l:[14,42,12,13,1337]
-    Test::eqadr((void *) 1337,(void *) *l->get(4));
-    Test::eqadr((void *) *l->get(l->getSize()-1),(void *) *l->get(4));
+    Test::eqadr((void *) 1337, (void *) *l->get(4));
+    Test::eqadr((void *) *l->get(l->getSize() - 1), (void *) *l->get(4));
 
-    *n= 6942069;
+    *n = 6942069;
     l->addLastCopy(n);
     //l:[14,42,12,13,1337,6942069]
-    Test::neqadr(n,l->get(l->getSize()-1));
+    Test::neqadr(n, l->get(l->getSize() - 1));
     delete n;
-    Test::eqadr((void *) 6942069,(void *) *l->get(l->getSize()-1));
+    Test::eqadr((void *) 6942069, (void *) *l->get(l->getSize() - 1));
 
 
-    Test::eqadr((void *) 6,(void *) l->getSize());
+    Test::eqadr((void *) 6, (void *) l->getSize());
     //gonna give undefined value
     /*delete m;
     Test::neqadr((void *) 1337,(void *) *l->get(4));
@@ -66,11 +62,36 @@ void testAdd(void* arg) {
 
 
 }
+//TODO implement tests
+
+void testRemove(void *arg);
+
+void testPop(void *arg);
+
+void testPos(void *arg);
+
+void testIterator(void *arg);
+
+void testGet(void *arg);
+
+void testClear(void *arg);
+
+void testSet(void *arg);
+
+void testReplace(void *arg);
+
+void testSort(void *arg);
+
+void testSize(void *arg);
+
+void testIsEmpty(void *arg);
+
+void testExists(void *arg);
 
 void testRun() {
     Test t(nullptr);
     //TODO add all functions
-    t.addTest("testAdd",testAdd);
+    t.addTest("testAdd", testAdd);
 
     t.run(nullptr);
     t.printResult();
